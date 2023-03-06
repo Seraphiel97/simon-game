61a2bec29f435eca1e86f89768251f336a549320
Seraphiel97 <seth.graves@outlook.com>
2023-03-06 11:25:42 -0500

    update js pseudocode

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
   /*----- constants -----*/
-
+// Create a constant to hold the colors the computer selects from
 
   /*----- state variables -----*/
-
+// Create a variable(array) to hold the master pattern
+// Create a variable(array) to hold the player's pattern
+// Create a winner variable set to null
 
   /*----- cached elements  -----*/
 
 
   /*----- event listeners -----*/
-
+// Create an event listener for the play button
+// Create an event listener for the play again/reset button
+// Create an event listener for the buttons that allow the player to select a color pattern
 
   /*----- functions -----*/
-
+// Function that creates a pattern stored in the master pattern variable
+// Function that iterates through the array, showing the colors to the player
+// Function that updates the array holding the player's choices
+// Function that compares the master pattern and player choices and declares a winner
+// Function that resets the game
+// Main render function
+// Render board function
+// Render controls function
+// Render message function
