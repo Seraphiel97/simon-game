d623d1e80ddb2cbfc386153aa53694df37531852
Seraphiel97 <seth.graves@outlook.com>
2023-03-06 18:21:16 -0500

    complete render controls

--- js/main.js
+++ js/main.js
@@ -11,10 +11,15 @@ const colors = {
   /*----- state variables -----*/
 // Variable(array) to hold the master pattern
 let correctPattern;
+
 // Variable(array) to hold the player's pattern
 let playerPattern;
+
 // Winner variable set to null
 let winner;
+
+// Variable to determine which part of the game the player is experiencing (0-play has not begun, 1-the computer creates and shows the pattern, 2-the player selects choices, 3-player solution is tested)
+let turn;
 
   /*----- cached elements  -----*/
 const play = document.getElementById('play');
@@ -25,17 +30,16 @@ const choicesArray = Array.from(choicesNodeList);
 
   /*----- event listeners -----*/
 // Create an event listener for the play button
-play.addEventListener('click', initialize);
+play.addEventListener('click', beginGame);
 // Create an event listener for the play again/reset button
-restart.addEventListener('click', initialize);
+restart.addEventListener('click', beginGame);
 // Create an event listener for the buttons that allow the player to select a color pattern
 
   /*----- functions -----*/
-
+initialize();
 // Function that initializes/resets the game 
 function initialize() {
-  makeCorrectPattern();
-  showCorrectPattern();
+  turn = 0;
   render();
 }
 
@@ -56,6 +60,13 @@ function showCorrectPattern() {
     }, idx * 1000)
   })
   setTimeout(() => compCircle.style.backgroundColor = 'white', 5000);
+}
+
+function beginGame() {
+  makeCorrectPattern();
+  showCorrectPattern();
+  turn += 1;
+  render();
 }
 
 // Function that updates the array holding the player's choices
@@ -80,8 +91,18 @@ function renderBoard() {
 };
 // Render controls function
 function renderControls() {
-
+if (turn === 0) {
+  play.style.visibility = 'visible';
+  restart.style.visibility = 'hidden';
+  choicesArray.forEach((choice) => choice.style.visibility = 'hidden');
+} else if (turn === 1) {
+  play.style.visibility = 'hidden';
+  choicesArray.forEach((choice) => choice.style.visibility = 'visible');
+} else if (turn === 2) {
+  play.style.visibility = 'hidden';
+  restart.style.visibility = 'visible';
 };
+}
 // Render message function
 function renderMessage() {
 
